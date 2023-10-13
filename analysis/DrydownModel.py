03bf7a320f7049757a8ae13bd2a9a1c48a361e2f
RY4GIT <400mhs2@gmail.com>
2023-10-12 17:02:38 -0700

    refactor:

--- analysis/DrydownModel.py
+++ analysis/DrydownModel.py
@@ -1,6 +1,4 @@
 from scipy.optimize import curve_fit
-from scipy.stats import norm
-from scipy.optimize import minimize
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -51,22 +49,14 @@ class DrydownModel:
 
     def fit_drydown_models(self, output_dir):
         """Fit the drydown models"""
-        # _____________________________________________
-        # Execute the analysis
+        self.output_dir = output_dir
 
         updated_events = []
 
         for i, event in enumerate(self.events.events()):
             try:
-                popt, r_squared, y_opt = self.fit_exponential(event)
-                event.add_attributes("exponential", popt, r_squared, y_opt)
-                popt, r_squared, y_opt = self.fit_q(event)
-                event.add_attributes("q", popt, r_squared, y_opt)
-
-                updated_events.append(event)
-
-                if self.plot_results:
-                    self.plot_drydown_models(event, output_dir)
+                updated_event = self.fit_one_event(event)
+                updated_events.append(updated_event)
 
             except Exception as e:
                 print(e)
@@ -75,7 +65,127 @@ class DrydownModel:
         # Replace the old Event instance with updated one
         self.events.events = updated_events
 
+    def fit_one_event(self, event):
+        # _____________________________________________
+        # Fit exponential model
+        popt, r_squared, y_opt = self.fit_exponential(event)
+        event.add_attributes("exponential", popt, r_squared, y_opt)
+
+        # _____________________________________________
+        # Fit q model
+        popt, r_squared, y_opt = self.fit_q(event)
+        event.add_attributes("q", popt, r_squared, y_opt)
+
+        # _____________________________________________
+        # Finalize results for one event
+        if self.plot_results:
+            self.plot_drydown_models(event)
+
+        return event
+
+    def fit_model(self, event, model, bounds, p0, norm=False):
+        try:
+            if norm:
+                y_fit = event.norm_y
+            else:
+                y_fit = event.y
+
+            # Fit the model
+            popt, _ = curve_fit(model, event.x, y_fit, p0, bounds)
+
+            # Get the optimal fit
+            y_opt = model(event.x, *popt)
+
+            if norm:
+                y_opt *= self.data.soil_moisture_range
+
+            # Calculate the residuals
+            residuals = event.y - y_opt
+            ss_res = np.sum(residuals**2)
+            r_squared = 1 - ss_res / np.sum((event.y - np.nanmean(event.y)) ** 2)
+
+            return popt, r_squared, y_opt
+
+        except Exception as e:
+            print("An error occurred:", e)
+
+    def fit_exponential_model(self, event):
+        """Fits an exponential model to the given event data and returns the fitted parameters.
+
+        Args:
+            event (EventData): An object containing event data.
+
+        Returns:
+            dict or None: A dictionary containing the fitted parameters and statistics, or None if an error occurs.
+        """
+
+        # ___________________________________________________________________________________
+        # Define the boundary condition for optimizing the exponential_model(t, delta_theta, theta_w, tau)
+
+        ### Delta_theta ###
+        min_delta_theta = 0
+        max_delta_theta = 2 * event.subset_sm_range
+        ini_delta_theta = 0.5 * event.subset_sm_range
+
+        ### Theta_w ###
+        min_theta_w = self.data.min_sm
+        max_theta_w = event.subset_min_sm
+        ini_delta_theta = (min_theta_w + max_theta_w) / 2
+
+        ### Tau ###
+        min_tau = 0
+        max_tau = np.inf
+        ini_tau = 1
+
+        bounds = [
+            (min_delta_theta, min_theta_w, min_tau),
+            (max_delta_theta, max_theta_w, max_tau),
+        ]
+        p0 = [ini_delta_theta, ini_delta_theta, ini_tau]
+
+        # ______________________________________________________________________________________
+        # Execute the event fit
+        return self.fit_model(self, event, exponential_model, bounds, p0, norm=False)
+
+    def fit_q_model(self, event):
+        """Fits a q model to the given event data and returns the fitted parameters.
+
+        Args:
+            event (EventData): An object containing event data.
+
+        Returns:
+            dict or None: A dictionary containing the fitted parameters and statistics, or None if an error occurs.
+        """
+
+        # ___________________________________________________________________________________
+        # Define the boundary condition for optimizing q_model(t, k, q, delta_theta)
+
+        ### k (should be equal to PET to reduce dimensionality ###
+        epsilon = 1.0e-64
+        min_k = event.pet - epsilon
+        max_k = event.pet + epsilon
+        ini_k = event.pet
+
+        ### q ###
+        min_q = 0.0
+        max_q = 10
+        ini_q = 1.0 + 1.0e-03
+
+        ### delta_theta ###
+        min_delta_theta = 0.0
+        max_delta_theta = 1.0
+        ini_delta_theta = 0.1
+
+        bounds = [(min_k, min_q, min_delta_theta), (max_k, max_q, max_delta_theta)]
+        p0 = [ini_k, ini_q, ini_delta_theta]
+
+        # ______________________________________________________________________________________
+        # Execute the event fit for the normalized timeseries between 0 and 1
+        return self.fit_model(self, event, q_model, bounds, p0, norm=True)
+
     def return_result_df(self):
+        """Return results in the pandas dataframe format for easier concatination"""
+
         results = []
         for event in self.events.events():
             _results = {
@@ -107,122 +217,14 @@ class DrydownModel:
 
         return df
 
-    def fit_exponential_model(self, event):
-        """Fits an exponential model to the given event data and returns the fitted parameters.
-
-        Args:
-            event (EventData): An object containing event data.
-
-        Returns:
-            dict or None: A dictionary containing the fitted parameters and statistics, or None if an error occurs.
-        """
-
-        # ___________________________________________________________________________________
-        # Define the bounds for the function exponential_model(t, delta_theta, theta_w, tau)
-
-        # Delta_theta
-        min_delta_theta = 0
-        max_delta_theta = 2 * event.subset_sm_range
-        ini_delta_theta = 0.5 * event.subset_sm_range
-
-        # Theta_w
-        min_theta_w = self.data.min_sm
-        max_theta_w = event.subset_min_sm
-        ini_delta_theta = (min_theta_w + max_theta_w) / 2
-
-        # Tau
-        min_tau = 0
-        max_tau = np.inf
-        ini_tau = 1
-
-        bounds = [
-            (min_delta_theta, min_theta_w, min_tau),
-            (max_delta_theta, max_theta_w, max_tau),
-        ]
-        p0 = [ini_delta_theta, ini_delta_theta, ini_tau]
-
-        # ___________________________________________________________________________________
-        try:
-            # Fit the data
-            # popt: Optimal values for the parameters so that the sum of the squared residuals of f(xdata, *popt) - ydata is minimized
-            # pcov: The estimated covariance of popt
-            popt, _ = curve_fit(
-                f=exponential_model, xdata=event.x, ydata=event.y, p0=p0, bounds=bounds
-            )
-
-            # Reroduce the analytical solution and calculate the residuals
-            y_opt = exponential_model(event.x, *popt)
-            residuals = event.y - y_opt
-            ss_res = np.sum(residuals**2)
-            ss_tot = np.sum((event.y - np.nanmean(event.y)) ** 2)
-            r_squared = 1 - (ss_res / ss_tot)
-
-            # Save the results
-            return popt, r_squared, y_opt
-
-        except Exception as e:
-            print("An error occurred:", e)
-
-    def fit_q_model(self, event):
-        """Fits a q model to the given event data and returns the fitted parameters.
-
-        Args:
-            event (EventData): An object containing event data.
-
-        Returns:
-            dict or None: A dictionary containing the fitted parameters and statistics, or None if an error occurs.
-        """
-
-        # Define the bounds
-        # q_model(t, k, q, delta_theta)
-        epsilon = 1.0e-64
-
-        min_k = event.pet - epsilon
-        max_k = event.pet + epsilon
-        ini_k = event.pet
-
-        min_q = 0.0
-        max_q = 10
-        ini_q = 1.0 + 1.0e-03
-
-        min_delta_theta = 0.0
-        max_delta_theta = 1.0
-        ini_delta_theta = 0.1
-
-        bounds = [(min_k, min_q, min_delta_theta), (max_k, max_q, max_delta_theta)]
-        p0 = [ini_k, ini_q, ini_delta_theta]
-
-        try:
-            # Fit the model
-            popt, _ = curve_fit(
-                f=q_model,
-                xdata=event.x,
-                ydata=event.norm_y,
-                p0=p0,
-                bounds=bounds,
-            )
-
-            # Reroduce the analytical solution and calculate the residuals
-            y_opt = q_model(event.x, *popt)
-
-            # Put back to original scale to calculate the residuals
-            residuals = event.y - y_opt * self.data.soil_moisture_range
-            ss_res = np.sum(residuals**2)
-            ss_tot = np.sum((event.y - np.nanmean(event.y)) ** 2)
-            r_squared = 1 - (ss_res / ss_tot)
-
-            return popt, r_squared, y_opt * self.data.soil_moisture_range
-
-        except Exception as e:
-            print("An error occurred:", e)
-            return None
-
-    def plot_drydown_models(self, event, output_dir):
+    def plot_drydown_models(self, event):
         ax = plt.plot()
 
+        # ______________________________________
         # Plot observed data
         ax.scatter(event.x, event.y)
 
+        # ______________________________________
         # Plot exponential model
         ax.plot(
             event.x[~np.isnan(event.y)],
@@ -231,6 +233,7 @@ class DrydownModel:
             label=f"expoential: R^2={event.exponential['r_squared']:.2f}; tau={event.exponential['tau']:.2f}",
         )
 
+        # ______________________________________
         # Plot q model
         ax.plot(
             event.x[~np.isnan(event.y)],
@@ -239,6 +242,7 @@ class DrydownModel:
             label=f"q model: R^2={event.q['r_squared']:.2f}; q={event.q['q']:.5f})",
         )
 
+        # ______________________________________
         # Plot PET
         ax2 = ax.twinx()
         ax2.scatter(event.x, event.pet, color="orange", alpha=0.5)
@@ -253,8 +257,10 @@ class DrydownModel:
         # Rotate the x tick labels
         ax.tick_params(axis="x", rotation=45)
 
+        # ___________________________________________________________________________________
+        # Save results
         filename = f"{self.data.EASE_row_index:03d}_{self.data.EASE_column_index:03d}_event_{self.index}.png"
-        output_subdir = os.path.join(output_dir, "plots")
+        output_subdir = os.path.join(self.output_dir, "plots")
         if ~os.path.exists(output_subdir):
             os.makedirs(output_subdir)
 
