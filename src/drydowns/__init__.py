a2492fe1c331b92fde1d05040d43b2b739a21a0c
Bryn Morgan <brynmorgan@ucsb.edu>
2024-04-14 22:35:37 -0700

    Fix imports

--- src/drydowns/__init__.py
+++ src/drydowns/__init__.py
@@ -6,7 +6,7 @@ from .toweragent import TowerAgent
 from .smapdata import SMAPData
 from .sensordata import SensorData, TowerSensorData, ISMNSensorData
 # from .towerdata import SoilSensorData
-from .ismndata import ISMNSoilData
+# from .ismndata import ISMNSoilData
 
 from .event import Event
 # from .towerevent import SensorEvent
